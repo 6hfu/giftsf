fdad2fcef93539b267f1c062e909b503681fe507
6hfu <167757120+6hfu@users.noreply.github.com>
2025-09-28 11:23:57 +0900

    Update app.py

--- app.py
+++ app.py
@@ -178,7 +178,7 @@ def submit():
         'X2__c', 'Field27__c', 'Field28__c', 'Field30__c', 'Field31__c',
         'Field41__c', 'Field39__c', 'Field35__c', 'Field36__c', 'Field37__c',
         'Field38__c', 'Field12__c', 'Field14__c', 'KDDI__c', 'KDDI1__c',
-        'NTT__c', 'NTT1__c', 'NTTX__c', 'Field184__c', 'Field229__c'
+        'NTT__c', 'NTT1__c', 'NTTX__c', 'Field184__c', 'Field229__c','Field271__c'
     ]
     form_data = {field: request.form.get(field) for field in import_fields}
     form_data['Field207__c'] = session.get('username', None)
