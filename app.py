c9ad9eb9aa8c39dfd9d6e3bbd18b42ca576612bb
6hfu <167757120+6hfu@users.noreply.github.com>
2025-10-08 11:29:50 +0900

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ sf = Salesforce(
 field76_map = {
     "AU光_007": "a05IU00001CHHNJYA5",
     "AU光_008": "a05IU00001CHHMwYAP",
-    "NURO光_002": "a05IU00001CHHNiYAP",
+    "AU光_010": "a05TL00000ncTETYA2",
     "NURO光_003": "a05TL00000ljIddYAE",
     "So-net光_002": "a05IU00001CHHNDYA5",
     "So-net光_004": "a05TL00000YGL4vYAH",
