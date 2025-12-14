5f4626c07559600e03a0312fdc8355261d046494
6hfu <167757120+6hfu@users.noreply.github.com>
2025-12-14 15:24:37 +0900

    Update app.py

--- app.py
+++ app.py
@@ -798,7 +798,7 @@ def update_record():
         if field25:
             t = datetime.strptime(field25, "%H:%M")
             dt_jst = datetime(2024, 1, 1, t.hour, t.minute)
-            dt_utc = dt_jst - timedelta(hours=19)  # JST → UTC
+            dt_utc = dt_jst - timedelta(hours=23)  # JST → UTC
             update_data['Field25__c'] = dt_utc.strftime("%H:%M:%S")
 
 
